d9a97e80070660d399f4c3404fbae252d6502470
Gilles FELIX <gilles@clubble.me>
2016-02-09 09:36:10 +0100

    Update PHPWS, websocket client now emit close event

--- app/classes/Bot2Hook/WebSocketClient.php
+++ app/classes/Bot2Hook/WebSocketClient.php
@@ -6,19 +6,6 @@ use Devristo\Phpws\Client\WebSocket;
 
 class WebSocketClient extends WebSocket
 {
-    public function open($timeOut=null)
-    {
-        $promise = parent::open($timeOut);
-
-        $this->on("connect", function () {
-            $this->stream->on('close', function() {
-                $this->emit('close');
-            });
-        });
-
-        return $promise;
-    }
-
     public function getState()
     {
         return $this->state;
