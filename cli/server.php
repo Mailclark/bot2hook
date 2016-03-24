5c230656e9b84bcee8fd0b859fc28bdc2ffbe381
Gilles FELIX <gilles@clubble.me>
2016-03-24 13:28:35 +0100

    Add config in notice server starting

--- cli/server.php
+++ cli/server.php
@@ -7,7 +7,7 @@ use Bot2Hook\Server;
 $config = require __DIR__.'/../app/bootstrap.php';
 
 $logger = new Logger($config['logger']);
-$logger->notice('Boot2Hook server starting');
+$logger->notice('Boot2Hook server starting', $config['server']);
 
 $rabbitmq = new Rabbitmq($config['rabbitmq']);
 $server = new Server($config['server'], $rabbitmq, $logger);
